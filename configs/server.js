33892de0ada68436ec20d544a6be779423ad4d69
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-06-17 11:15:19 -0600

    realizamos server y conexión a mongo

---
+++ configs/server.js
@@ -0,0 +1,51 @@
+'use strict'
+
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import { dbConnection } from './mongo.js';
+/*import userRoutes from '../src/users/user.routes.js';
+import authRoutes from '../src/auth/auth.routes.js';
+import companyRoutes from '../src/companies/company.routes.js';*/
+
+
+class Server{
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+        /*this.usuarioPath = '/companyControl/v1/users'
+        this.authPath = '/companyControl/v1/auth'
+        this.companyPath = '/companyControl/v1/company'*/
+
+        this.middlewares();
+        this.conectarDB();
+        //this.routes();
+    }
+
+    async conectarDB(){
+        await dbConnection();
+    }
+
+    middlewares(){
+        this.app.use(express.urlencoded({extended: false}));
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(helmet());
+        this.app.use(morgan('dev'));
+    }
+
+    /*routes(){
+        this.app.use(this.authPath, authRoutes);
+        this.app.use(this.usuarioPath, userRoutes);
+        this.app.use(this.companyPath, companyRoutes);
+    }*/
+
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log('Server running on port ', this.port);
+        });
+    }
+}
+
+export default Server;

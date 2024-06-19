e61fac3688b1719d9a12b44b6c64f91977f345e3
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-06-19 12:23:36 -0600

    router post, get y getById de usuario

---
+++ src/user/user.routes.js
@@ -0,0 +1,43 @@
+import { Router } from "express";
+import { check } from "express-validator";
+import {
+    usuarioPost,
+    usuarioGet,
+    getUsuarioById,
+    usuarioPut,
+    usuarioDelete
+} from './user.controller.js';
+
+import {
+    existenteEmail,
+    existeUsuarioById,
+} from '../helpers/db-validators.js';
+
+import { validarCampos } from '../middlewares/validar-campos.js';
+
+const router = Router();
+
+router.get('/', usuarioGet);
+
+router.post(
+    "/",
+    [
+        check("nombre", "El nombre es obligatorio").not().isEmpty(),
+        check("password", "La contraseña es obligatoria").not().isEmpty(),
+        check("password", "La contraseña debe ser más de 6 letras".italics()).isLength({ min: 6 }),
+        check("correo", "El correo no es válido").isEmail(),
+        check("correo").custom(existenteEmail),
+        validarCampos,
+    ],
+    usuarioPost
+);
+
+router.get(
+    "/:id",
+    [
+        check("id", "No es un ID válido").isMongoId(),
+        check("id").custom(existeUsuarioById),
+        validarCampos
+    ],
+    getUsuarioById
+);

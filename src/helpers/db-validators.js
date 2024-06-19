0d962749c0640641e6815fbffc79acaa715dd10b
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-06-19 11:16:42 -0600

    se agrega validator de correo y existencia de usuarioById

--- src/helpers/db-validators.js
+++ src/helpers/db-validators.js
@@ -1,1 +1,15 @@
-import 
+import Usuario from '../user/user.model.js'
+
+export const existenteEmail = async (correo = '') => {
+    const existeEmail = await Usuario.findOne({ correo }); 
+    if (existeEmail) {
+        throw new Error(`El correo ${correo} ya está registrado`);
+    }
+};
+
+export const existeUsuarioById = async (id) => {
+    const existeUsuario = await Usuario.findById(id);
+    if(!existeUsuario){
+        throw new Error(`El id ${id} no existe`);
+    }
+};

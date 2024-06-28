70a15592781a0b48251b37ba0ec13e68d3a9630a
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-06-27 18:14:52 -0600

    funcion get recursos

--- src/recursos/recurso.controller.js
+++ src/recursos/recurso.controller.js
@@ -9,3 +9,16 @@ export const recursosPost = async (req, res) => {
     res.status(201).json({ recurso });
 };
 
+
+export const recursoGet = async (req, res) => {
+    const { limite = 5, desde = 0 } = req.query;
+
+    const [total, recurso] = await Promise.all([
+        Recurso.countDocuments(),
+        Recurso.find()
+            .skip(Number(desde))
+            .limit(Number(limite))
+    ]);
+
+    res.status(200).json({ total, recurso });
+};

f4ed678866d2c6088047ef09214595f6f4652c70
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-06-28 15:49:28 -0600

    se corrige modelo de diario, se agrega fecha, y arreglo de entradas

--- src/diario/diario.model.js
+++ src/diario/diario.model.js
@@ -1,17 +1,25 @@
 import mongoose from 'mongoose';
 
 const DiarioSchema = new mongoose.Schema({
-    usuario: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Usuario',
-        required: true
-    },
-    entrada: {
+  usuario: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Usuario',
+    required: true
+  },
+  fecha: {
+    type: String,
+    required: true,
+  },
+  entradas: [
+    {
+      contenido: {
         type: String,
-        required: [true, 'La entrada es obligatoria'],
+        required: true
+      }
     }
+  ]
 }, {
-    timestamps: true,
+  timestamps: true,
 });
 
 export default mongoose.model('Diario', DiarioSchema);

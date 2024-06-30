58e793b0e83036659e49a539d50857fa483bccbf
oflores-2022234 <oflores-2022234@kinal.edu.gt>
2024-06-30 15:01:30 -0600

    add in the controller of message the function getConversacionByDate

--- src/mensajes/mensaje.controller.js
+++ src/mensajes/mensaje.controller.js
@@ -61,3 +61,23 @@ export const getConversaciones = async (req, res) => {
         conversaciones
     });
 }
+
+export const getConversacionesByDate = async (req, res) => {
+    const { usuarioId, fecha } = req.params;
+    const remitenteId = req.usuario._id;
+
+    const conversacion = await Conversacion.findOne({
+        usuarios: { $all: [remitenteId, usuarioId] },
+        fecha
+    }).poputalte('usuarios', '_id nombre').populate('mensajes.remitente', '_id nombre');
+
+    if(!conversacion){
+        return res.status(404).json({
+            msg: 'Conversation not found for this date'
+        });
+    }
+
+    res.status(200).json({
+        conversacion
+    });
+}
